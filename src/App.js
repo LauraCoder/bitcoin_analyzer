66e0e140a8b62f4fc0e9a245b46877e16903ed75
LauraCoder <laura.pelkonen@student.laurea.fi>
2021-12-16 18:37:05 +0200

    More bug fixing, and adding loading indicator.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import InvestmentAnalyzer from './components/InvestmentAnalyzer'
 import Navbar from './components/Navbar'
 
 const App = () => {
+  const [ isLoading, setIsLoading ] = useState(false)
   const [ cryptoInfo, setCryptoInfo ] = useState([])
   const [ startDate, setStartDate ] = useState(null)
   const [ endDate, setEndDate ] = useState(null)
@@ -28,6 +29,7 @@ const App = () => {
   }
 
   useEffect(() => {
+    setIsLoading(true)
     if ( startDate && endDate ) {
       //"Above 90 days from query time = daily data (00:00 UTC)"
       //"1 - 90 days from query time = hourly data"
@@ -43,7 +45,7 @@ const App = () => {
           setCryptoInfo(initialData)
         })
         // eslint-disable-next-line no-unused-vars
-        .catch(error => {
+        .catch(_error => {
           setMessage(
             'Something went wrong'
           )
@@ -52,6 +54,7 @@ const App = () => {
           }, 3000)
         })
     }
+    setIsLoading(false)
   }, [endDate, startDate])
 
   const dailyPrice = () => {
@@ -59,64 +62,70 @@ const App = () => {
 
     //Above 90 days from query time = daily data (00:00 UTC)
     if( diffStartDateEndDate > ninetyDaysUnix ) {
-      return dailyPriceArray
+      return (dailyPriceArray)
     }
 
     //1 - 90 days from query time = hourly data
     let newArr = [dailyPriceArray[0]]
-    const filteredArray = dailyPriceArray.slice(1).filter((i=1) => i % 24 === 24 - 1)
+    const filteredArray = dailyPriceArray.slice(1).filter((_e, i=1) => i % 24 === 24 - 1)
     newArr.push(...filteredArray)
 
     return newArr
   }
 
   return (
-    <div className='container'>
-      <Notification message={message} />
-      <div className='row'>
-        <div className='column'>
-          <h1>Bitcoin Analyzer</h1>
-        </div>
-      </div>
-      <div className='row'>
-        <div className='column'>
-          <DateForm
-            show={show}
-            setShow={setShow}
-            setPage={setPage}
-            startDate={startDate}
-            setStartDate={setStartDate}
-            endDate={endDate}
-            setEndDate={setEndDate}
-            setMessage={setMessage}
-          />
-        </div>
-      </div>
-      {show &&
-        <div className='dataColumn'>
-          <Navbar
-            page={page}
-            setPage={setPage}
-          />
+    <>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) :
+        <div className='container'>
+          <Notification message={message} />
           <div className='row'>
             <div className='column'>
-              <BearishTrend
-                show={page === 'bearishTrend'}
-                dailyPrice={dailyPrice}
-              />
-              <TradingVolume
-                show={page === 'tradingVolume'}
-                cryptoInfo={cryptoInfo}
-              />
-              <InvestmentAnalyzer
-                show={page === 'investmentAnalyzer'}
-                dailyPrice={dailyPrice}
+              <h1>Bitcoin Analyzer</h1>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='column'>
+              <DateForm
+                show={show}
+                setShow={setShow}
+                setPage={setPage}
+                startDate={startDate}
+                setStartDate={setStartDate}
+                endDate={endDate}
+                setEndDate={setEndDate}
+                setMessage={setMessage}
               />
             </div>
           </div>
+          {show &&
+            <div className='dataColumn'>
+              <Navbar
+                page={page}
+                setPage={setPage}
+              />
+              <div className='row'>
+                <div className='column'>
+                  <BearishTrend
+                    show={page === 'bearishTrend'}
+                    dailyPrice={dailyPrice}
+                  />
+                  <TradingVolume
+                    show={page === 'tradingVolume'}
+                    cryptoInfo={cryptoInfo}
+                  />
+                  <InvestmentAnalyzer
+                    show={page === 'investmentAnalyzer'}
+                    dailyPrice={dailyPrice}
+                  />
+                </div>
+              </div>
+            </div>
+          }
         </div>
       }
-    </div>
+    </>
   )
 }
 
